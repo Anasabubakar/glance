9aaab87ba52dab39b0b08a5923972ae78eda5688
Anas Abubakar <anasabubakar7000@gmail.com>
2026-07-14 16:39:28 +0100

    feat: add site constants

---
+++ frontend/src/lib/constants.ts
@@ -0,0 +1,8 @@
+export const SITE = {
+  name: "Glance",
+  description: "Your Intelligent Cursor",
+  url: "https://glance.dev",
+  github: "https://github.com/Anasabubakar/glance",
+} as const;
+
+export const HOTKEY = "Ctrl + Alt + M";
